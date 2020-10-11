535f1e8137bb5ccbdbce2651c50f3513303a677a
Tony Rundström <Breckie1@users.noreply.github.com>
2020-10-11 14:24:23 +0200

    Created a sine movement to an enemy and an attack pattern based on distance

---
+++ RobotInfection/Assets/Script/Bullets/RNABullet.cs
@@ -0,0 +1,21 @@
+﻿
+
+public class RNABullet : Bullet
+{
+	private float _rnaBulletSpeed = 20f;
+	public void Start()
+	{
+		bulletSpeed = _rnaBulletSpeed;
+		bulletElement = Elements.NONE;
+	}
+	public Elements BulletProperties(Elements element)
+	{
+		element = Elements.NONE;
+		return element;
+	}
+	public float BulletSpeed()
+	{
+		bulletSpeed = _rnaBulletSpeed;
+		return _rnaBulletSpeed;
+	}
+}
--- RobotInfection/Assets/Script/Enemy/Enemies/TVirus.cs
+++ RobotInfection/Assets/Script/Enemy/Enemies/TVirus.cs
@@ -15,23 +15,28 @@ public class TVirus : MonoBehaviour
 	 * the player and then spiral 
 	 * in towards the player.
 	 */
-	public GameObject _player;
-	public float _sineCurveDistance;
-
+	private GameObject _player;
+	private GameObject _rnaBullet;
+	private float _sineCurveDistance = 5;
 	private ObjectMovement _objectMovement;
 	private ObjectRotation _objectRotation;
 	private State _state;
-	public float _deltaTime;
-	public Vector2 _movement;
-	public Vector2 _position;
-	public float _movementSpeed = 1f;
+	private float _deltaTime;
+	private Vector2 _movement;
+	private Vector2 _position;
+	private float _movementSpeed = 1f;
 	private float _x;
 	private float _y;
-	public float _distanceToTarget;
+	private float _distanceToTarget = 4;
+	private float _fireRate = .3f;
+	private float _timer;
 	private Vector3 _playerPosition;
 
 	private void Start()
 	{
+		_player = GameObject.FindGameObjectWithTag("Player");
+		_rnaBullet = Resources.Load<GameObject>("Prefabs/RNABullet");
+
 		_movement = new Vector2(1f, 1f);
 		_objectMovement = GetComponent<ObjectMovement>();
 		_objectRotation = GetComponent<ObjectRotation>();
@@ -45,6 +50,8 @@ public class TVirus : MonoBehaviour
 		 * he is a much better coder than I am, so it is 
 		 * basically me trying to improve. 
 		 */
+		_deltaTime = Time.deltaTime;
+
 		switch (_state)
 		{
 			case State.MOVING:
@@ -57,32 +64,7 @@ public class TVirus : MonoBehaviour
 	}
 	private void UpdateMovement()
 	{
-		//_deltaTime = Time.deltaTime;
-		//_position.y = _player.transform.position.y - transform.position.y;
-		//_position.x = _player.transform.position.x - transform.position.x;
-		//_position.x = Mathf.Cos(_position.x);
-		//_position.y = Mathf.Sin(_position.y);
-		//if (_position.y > _sineCurveDistance && _movement.y > 0)
-		//{
-		//	_movement.y *= -1;
-		//}
-		//if (_position.y < _sineCurveDistance && _movement.y < 0)
-		//{
-		//	_movement.y *= -1;
-		//}
-		//if (_position.x < 0 && _movement.x > 0)
-		//{
-		//	_position.x *= -1;
-		//}
-		//if (_position.x > 0 && _movement.x < 0)
-		//{
-		//	_position.x *= -1;
-		//}
-		//_objectMovement.Movement(_movement.x, _movement.y, _deltaTime, _movementSpeed);
-		_deltaTime = Time.deltaTime;
-		_playerPosition = _player.transform.position;
-		_x = _playerPosition.x - transform.position.x;
-		_y = _playerPosition.y - transform.position.y;
+		PlayerPosition();
 		if (_x < 0)
 		{
 			_movement.x = -0.1f;
@@ -99,7 +81,7 @@ public class TVirus : MonoBehaviour
 		{
 			_movement.y = 0.1f;
 		}
-		if (Mathf.Sqrt( Mathf.Pow(_x,2) + Mathf.Pow(_y, 2)) < _distanceToTarget)
+		if (Mathf.Pow((Mathf.Pow(_x, 2) + Mathf.Pow(_y, 2)), 0.5f) < _distanceToTarget)
 		{
 			_state = State.ATTACKING;
 		}
@@ -108,6 +90,23 @@ public class TVirus : MonoBehaviour
 	}
 	private void Attacking()
 	{
-		Debug.Log("Attacking");
+		PlayerPosition();
+		_timer += _deltaTime;
+		if (_timer > _fireRate)
+		{
+			_objectRotation.FollowGameObject(_player);
+			Instantiate(_rnaBullet, transform.position, gameObject.transform.rotation);
+			_timer = 0;
+		}
+		if (Mathf.Pow((Mathf.Pow(_x, 2) + Mathf.Pow(_y, 2)), 0.5f) > _distanceToTarget)
+		{
+			_state = State.MOVING;
+		}
+	}
+	private void PlayerPosition()
+	{
+		_playerPosition = _player.transform.position;
+		_x = _playerPosition.x - transform.position.x;
+		_y = _playerPosition.y - transform.position.y;
 	}
 }
